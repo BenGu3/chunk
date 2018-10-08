59434ea399a41ac39ce1e9bfda946b8d841c7724
bengu3 <benjee.012@gmail.com>
2018-10-07 22:22:57 -0600

    Add bottom nav layout

--- App.js
+++ App.js
@@ -1,40 +1,71 @@
-import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react'
+import { View } from 'react-native'
+import BottomNavigation, { IconTab } from 'react-native-material-bottom-navigation'
+import Icon from 'react-native-vector-icons/Ionicons';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import Calendar from './src/components/calendar'
 
 export default class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeTab: 'calendar'
+    }
+  }
+
+  tabs = [
+    {
+      key: 'menu',
+      icon: 'md-menu',
+      barColor: '#ffffff',
+      pressColor: 'rgba(0, 0, 0, 0.16)'
+    },
+    {
+      key: 'calendar',
+      icon: 'md-calendar',
+      barColor: '#ffffff',
+      pressColor: 'rgba(0, 0, 0, 0.16)'
+    },
+    {
+      key: 'add',
+      icon: 'md-add',
+      barColor: '#ffffff',
+      pressColor: 'rgba(0, 0, 0, 0.16)'
+    }
+  ]
+
+  handleTabPress = (newTab, oldTab) => {
+    this.setState({ activeTab: newTab.key })
+  }
+
+  renderIcon = iconName => ({ isActive }) => {
+    return <Icon size={24} color="black" name={iconName}/>
+  }
+
+  renderTab = ({ tab, isActive }) => {
+    return (
+      <IconTab
+        key={tab.key}
+        isActive={isActive}
+        renderIcon={this.renderIcon(tab.icon)}
+      />
+    )
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+      <View style={{ flex: 1 }}>
+        <View style={{ flex: 1 }}>
+          <Calendar/>
+        </View>
+        <BottomNavigation
+          activeTab={this.state.activeTab}
+          onTabPress={this.handleTabPress}
+          renderTab={this.renderTab}
+          tabs={this.tabs}
+        />
       </View>
-    );
+    )
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
