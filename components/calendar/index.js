aeab68277aadb93fdf03a43fc606c94a88d850ad
bengu3 <benjee.012@gmail.com>
2018-10-09 12:45:55 -0600

    Add add buttons on Calendar and ChunkList screens

--- components/calendar/index.js
+++ components/calendar/index.js
@@ -1,24 +1,38 @@
 import React from 'react'
 import { Platform, StyleSheet, Text, View } from 'react-native';
+import { FAB } from 'react-native-paper';
 
-export default class Index extends React.Component {
+export default class Calendar extends React.Component {
   constructor(props) {
     super(props)
   }
 
-  componentDidMount() {
-
-  }
-
-  componentWillUnmount() {
-
+  handleOnPress() {
+    console.log('Calendar Add Pressed')
   }
 
   render() {
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
         <Text style={{ textAlign: 'center' }}>Calendar</Text>
+        <FAB
+          style={styles.fab}
+          color='white'
+          small
+          icon="add"
+          onPress={this.handleOnPress()}
+        />
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  fab: {
+    backgroundColor: '#4646FF',
+    position: 'absolute',
+    margin: 16,
+    right: 0,
+    bottom: 0,
+  },
+})
--- components/chunk-list/index.js
+++ components/chunk-list/index.js
@@ -1,24 +1,38 @@
 import React from 'react'
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+import { FAB } from 'react-native-paper';
 
 export default class ChunkList extends React.Component {
   constructor(props) {
     super(props)
   }
 
-  componentDidMount() {
-
-  }
-
-  componentWillUnmount() {
-
+  handleOnPress() {
+    console.log('ChunkList Add Pressed')
   }
 
   render() {
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
         <Text style={{ textAlign: 'center' }}>ChunkList</Text>
+        <FAB
+          style={styles.fab}
+          color='white'
+          small
+          icon="add"
+          onPress={this.handleOnPress()}
+        />
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  fab: {
+    backgroundColor: '#4646FF',
+    position: 'absolute',
+    margin: 16,
+    right: 0,
+    bottom: 0,
+  },
+})
