ffbdfdebe7bf59947e1da4f4a16699e39f85a5db
bengu3 <benjee.012@gmail.com>
2018-10-19 14:53:24 -0600

    Add shadow to bottom nav

--- src/components/bottom-nav/index.js
+++ src/components/bottom-nav/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import Icon from 'react-native-vector-icons/Ionicons';
-import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs';
+import { createBottomTabNavigator, BottomTabBar } from 'react-navigation-tabs'
 
 import Calendar from '../calendar'
 import TaskList from '../task-list'
 
-export default createMaterialBottomTabNavigator({
+export default createBottomTabNavigator({
   TaskList: {
     screen: TaskList,
     navigationOptions: {
       tabBarIcon: ({ tintColor, focused }) => (
-        <Icon size={25} name={'md-list'} style={{ color: tintColor }} />
+        <Icon size={25} name={'md-list'} style={{ color: tintColor }}/>
       )
     }
   },
@@ -18,14 +18,23 @@ export default createMaterialBottomTabNavigator({
     screen: Calendar,
     navigationOptions: {
       tabBarIcon: ({ tintColor, focused }) => (
-        <Icon size={28} name={'md-calendar'} style={{ color: tintColor }} />
+        <Icon size={28} name={'md-calendar'} style={{ color: tintColor }}/>
       )
     }
   },
 }, {
   initialRouteName: 'Calendar',
-  activeColor: '#4646FF',
-  inactiveColor: '#373737',
-  barStyle: { backgroundColor: '#ffffff' },
-  labeled: false
+  tabBarOptions: {
+    activeColor: '#4646FF',
+    inactiveColor: '#373737',
+    style: {
+      backgroundColor: '#ffffff',
+      borderTopWidth: 0,
+      shadowOpacity: 0.75,
+      shadowRadius: 3,
+      shadowColor: 'black',
+      shadowOffset: { height: 0, width: 0 },
+    },
+    showLabel: false
+  }
 })
